8ccb4b20913835c7835a05eaee2af7ef2a9a8e3a
swissquant@gmail.com <aym.germain@gmail.com>
2022-11-08 12:57:47 +0000

    :sparkles: add squirrel contract and dependencies

---
+++ contracts/squirrel.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.7.6;
+
+// import "@chainlink/contracts/src/v0.6/vendor/SafeMathChainlink.sol";
+// import {IERC721} from "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import "@squeeth/packages/hardhat/contracts/core/Controller.sol";
+
+contract Squirrel {
+    Controller controller;
+
+    function shortPowerPerp() public returns (uint256) {
+        return controller.shortPowerPerp();
+    }
+}
