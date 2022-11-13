c02b0c36c3b76fc0f79dfa654c6446add161a23c
swissquant@gmail.com <aym.germain@gmail.com>
2022-11-13 11:17:19 +0000

    :wrench: change function to view

--- contracts/squirrel.sol
+++ contracts/squirrel.sol
@@ -1,14 +1,12 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.7.6;
 
-// import "@chainlink/contracts/src/v0.6/vendor/SafeMathChainlink.sol";
-// import {IERC721} from "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import "@squeeth/packages/hardhat/contracts/core/Controller.sol";
 
 contract Squirrel {
     Controller controller;
 
-    function shortPowerPerp() public returns (address) {
+    function shortPowerPerp() public view returns (address) {
         return controller.shortPowerPerp();
     }
 }
