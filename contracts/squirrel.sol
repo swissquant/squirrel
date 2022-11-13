c6a5675b2a818ac7abb453c2141735353058e36b
swissquant@gmail.com <aym.germain@gmail.com>
2022-11-13 11:23:53 +0000

    :wrench: adding address of the controller

--- contracts/squirrel.sol
+++ contracts/squirrel.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.7.6;
 import "@squeeth/packages/hardhat/contracts/core/Controller.sol";
 
 contract Squirrel {
-    Controller controller;
+    Controller controller = Controller(0x50f3D0826d4E3c3d49007DBa664727B9885Dd734);
 
     function shortPowerPerp() public view returns (address) {
         return controller.shortPowerPerp();
