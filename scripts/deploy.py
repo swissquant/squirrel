30daa6889852e6f08ef61fdf58e41d2805cb1500
swissquant@gmail.com <aym.germain@gmail.com>
2022-11-13 11:17:04 +0000

    :sparkles: add etherscan verification

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -2,4 +2,4 @@ from brownie import Squirrel, accounts
 
 def main():
     account = accounts.load('dev')
-    Squirrel.deploy({'from': account})
+    Squirrel.deploy({'from': account}, publish_source=True)
