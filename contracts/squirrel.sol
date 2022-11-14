600bf1c38313b51b7c3d479b6d3b5ed8fc063324
swissquant@gmail.com <aym.germain@gmail.com>
2022-11-14 19:19:08 +0000

    :sparkles: final version of maxMintPowerPerp

--- contracts/squirrel.sol
+++ contracts/squirrel.sol
@@ -1,20 +1,21 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.7.6;
 
+import {SafeMath} from "@openzeppelin/contracts/math/SafeMath.sol";
+
 import {VaultLib} from "@squeeth/packages/hardhat/contracts/libs/VaultLib.sol";
 import {Controller} from "@squeeth/packages/hardhat/contracts/core/Controller.sol";
 import {Power2Base} from "@squeeth/packages/hardhat/contracts/libs/Power2Base.sol";
 
-import {SafeMath} from "@openzeppelin/contracts/math/SafeMath.sol";
-
 contract Squirrel {
     // Libraries
+    using SafeMath for uint96;
     using SafeMath for uint256;
 
     // Contract variables
     Controller controller;
+    uint256 constant ONE = 1e18;
     uint256 constant ONE_ONE = 1e36;
-    mapping(uint256 => VaultLib.Vault) public vaults;
 
     // Constructor
     constructor(
@@ -39,17 +40,34 @@ contract Squirrel {
         return _scaledEthPrice;
     }
 
-    // In construction
-    function maxMintPowerPerp(uint256 vaultId) public view returns (uint256) {
-        VaultLib.Vault memory vault = vaults[vaultId];
-        uint256 normalizationFactor = controller.getExpectedNormalizationFactor();
-        uint256 ethQuoteCurrencyPrice = 0;
-        uint256 debtValueInETH = uint256(vault.shortAmount).mul(normalizationFactor).mul(ethQuoteCurrencyPrice).div(ONE_ONE);
+    /*
+    Fetch the details of a vault by vaultId
+    */
+    function getVault(uint256 vaultId) internal view returns (VaultLib.Vault memory) {
+        address operator;
+        uint32 NftCollateralId;
+        uint96 collateralAmount;
+        uint128 shortAmount;
+        (operator, NftCollateralId, collateralAmount, shortAmount) = controller.vaults(vaultId);
 
-        return normalizationFactor;
+        return VaultLib.Vault(operator, NftCollateralId, collateralAmount, shortAmount);
     }
 
-    function shortPowerPerp() public view returns (address) {
-        return controller.shortPowerPerp();
+    /*
+    Calculate the maximum amount of squeeth that is possible to mint given the current collateralisation of the vault
+    */
+    function maxMintPowerPerp(uint256 vaultId) public view returns (uint256) {
+        // Fetching the ingredients for the upcoming calculations
+        VaultLib.Vault memory vault = getVault(vaultId);
+        uint256 normalizationFactor = controller.getExpectedNormalizationFactor();
+        uint256 ethPrice = getEthPrice();
+
+        // Calculating the debt of the vault in ETH
+        uint256 debtValueInETH = uint256(vault.shortAmount).mul(normalizationFactor).mul(ethPrice).div(ONE_ONE);
+        
+        // Calculating the amount of squeeth that is possible to mint given the current status of the vault
+        uint256 powerPerp = vault.collateralAmount.mul(2).div(ethPrice.mul(normalizationFactor).mul(3)).mul(ONE_ONE) - debtValueInETH;
+
+        return powerPerp;
     }
 }
