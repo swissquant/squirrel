e05a533ba4d4c0ebd57ce9b96ab22b351484b1c7
swissquant@gmail.com <aym.germain@gmail.com>
2022-11-14 16:02:53 +0000

    :sparkles: improved constructor and controller initialisation

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -3,10 +3,7 @@ from brownie import Squirrel, accounts
 def main():
     account = accounts.load('dev')
     Squirrel.deploy(
-        "0x65D66c76447ccB45dAf1e8044e918fA786A483A1",
-        "0x8ad599c3A0ff1De082011EFDDc58f1908eb6e6D8",
-        "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
-        "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48",
+        "0x50f3D0826d4E3c3d49007DBa664727B9885Dd734", # Controller's address
         {'from': account},
         publish_source=True,
     )
